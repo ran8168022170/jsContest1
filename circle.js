b64ad399287b2b7bb3913af395169aaa4a1f37a4
ran8168022170 <ranjeetkanu77@gmail.com>
2022-12-04 14:51:40 +0530

    contest

---
+++ circle.js
@@ -0,0 +1,8 @@
+import Shape from "./shape.js"
+
+
+class Circle extends Shape{
+    calculateArea(){
+        console.log("Area of Circle");
+    }
+}
---
+++ shape.js
@@ -0,0 +1,15 @@
+class Shape{
+
+    constructor(color){
+        this.color=color;
+    }
+    calculateArea(){
+        console.log("Area of shape");
+
+    }
+    drawShape(){
+        console.log("Hello shape");
+    }
+}
+
+module.export=Shape;
